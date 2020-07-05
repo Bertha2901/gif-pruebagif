7355587cf25c04bc5cf40281f6e8a9da618f969e
Bertha <berthaturcios3@gmail.com>
2020-07-04 21:28:09 -0600

    Prueba git

--- src/pruebagif.cpp
+++ src/pruebagif.cpp
@@ -3,11 +3,13 @@
  int main(){
 
     string nombre;
-    int edad;
+    int edad,telefono;
     cout<<"Por favor ingrese su nombre completo:"<<endl;
     getline(cin,nombre);
     cout<<"Ingrese su edad: ";
     cin>>edad;
+    cout<<"Ingrese su numero de telefono movil: "<<endl;
+    cin>>telefono;
     system("PAUSE");
      return 0;
  }
