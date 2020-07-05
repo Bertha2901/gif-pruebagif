797d5e8dedcb018ea8645e0d41b7a7b49780741f
Bertha <berthaturcios3@gmail.com>
2020-07-04 18:09:46 -0600

    Prueba de gif

---
+++ src/pruebagif.cpp
@@ -0,0 +1,13 @@
+ #include <iostream>
+ using namespace std;
+ int main(){
+
+    string nombre;
+    int edad;
+    cout<<"Por favor ingrese su nombre completo:"<<endl;
+    getline(cin,nombre);
+    cout<<"Ingrese su edad: ";
+    cin>>edad;
+    system("PAUSE");
+     return 0;
+ }
